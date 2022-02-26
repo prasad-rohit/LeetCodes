81fb8c048351475989c3f9ee708be2f91133918d
Rohit Prasad <52075065+prasad-rohit@users.noreply.github.com>
2022-02-26 12:50:13 +0530

    Time: 3 ms (80.90%), Space: 6 MB (25.99%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        return floor(-0.5+sqrt((double)2*n+0.25));
+        int l=0,r=n,ans;
+        
+        long row,coinneed;
+        while(l<=r){
+            row = l + ((r-l)/2);
+            coinneed = (row*(row+1))/2;
+            
+            if(coinneed <=n)  l = row+1, ans = row;
+            else r= row-1;
+        }
+        
+        return ans;
     }
 };
